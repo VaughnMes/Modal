82edbf390042cc6981a27ceba91fa2691a6c47a9
Vaughn Mesquita <63569058+VaughnMes@users.noreply.github.com>
2024-01-17 22:42:39 -0500

    Title: Incorporated a btn to open/close the modal (#2)
    Desc: While the JS code works, on a minimum basis, to
    open and close the modal. For some reason, once the button is
    pressed to close and reopen, my styling/alignment is off. I'm
    not sure why - will have to investigate.

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 
-document.addEventListener (
-    "click",
-    function(event) {
-        if (
-            event.target.matches('.fa-solid.fa-square-xmark') ||
-            event.target.matches('.close-btn') ||
-            !event.target.closest('.modal')
-        ) {
-            closeModal();
-        }
-    }, 
-    false
-)
 
-function closeModal() {
-    document.querySelector('.modal').style.display = 'none';
+function openCloseModal() {
+    let modal = document.querySelector(".modal");
+    if (modal.style.display === "none") {
+        modal.style.display = "block";
+    } else {
+        modal.style.display = "none";
+    }
 }
+
+
+
+
+
+// document.addEventListener (
+//     "click",
+//     function(event) {
+//         if (
+//             event.target.matches('.fa-solid.fa-square-xmark') ||
+//             event.target.matches('.close-btn') ||
+//             !event.target.closest('.modal')
+//         ) {
+//             closeModal();
+//         }
+//     }, 
+//     false
+// )
+
+// function closeModal() {
+//     document.querySelector('.modal').style.display = 'none';
+// }
+
